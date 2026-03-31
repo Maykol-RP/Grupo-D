4404e17f3448befe46eeba104e47f839bf024de6
Maykol-RP <erteanti@gmail.com>
2026-03-31 16:40:48 -0500

    Proyecto final funcional

--- fray.js
+++ fray.js
@@ -2,16 +2,3 @@ function girar(tarjeta) {
   const inner = tarjeta.querySelector('.tarjeta-inner');
   inner.classList.toggle('girado');
 }
-
-window.addEventListener("scroll", function () {
-  const elementos = document.querySelectorAll(".reveal");
-
-  elementos.forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    const visible = window.innerHeight - 100;
-
-    if (top < visible) {
-      el.classList.add("active");
-    }
-  });
-});
